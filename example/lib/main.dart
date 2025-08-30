e7eda841c0c41188a30877b638529d1fe68dc6a1
Parker Roth <parkeroth@gmail.com>
2025-08-30 14:01:49 -0500

    Simplify the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:collection/collection.dart';
-import 'package:fc_core/logic/cycle_rendering.dart';
-import 'package:fc_core/logic/observation_parser.dart';
-import 'package:fc_core/models/chart.dart';
-import 'package:fc_core/widgets/chart_widget.dart';
-import 'package:fc_core/widgets/cycle_widget.dart';
+import 'package:fc_core/widgets/simple_chart_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:time_machine/time_machine.dart';
+
+const observations = ["H", "M", "0 AD", "10 CK X2", "0 AD", "2 AD", "6CG X3"];
 
 void main() {
   runApp(const MyApp());
@@ -27,59 +24,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final ChartController _chartController = ChartController(
-    startOfCharting: LocalDate.today(),
-  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: ChartWidget(
-          includeFooter: true,
-          chart: Chart.fromCycles([cycle], 6).first,
-          controller: _chartController,
-          rightWidgetFn: (c) => null,
+        child: SimpleChartWidget(
+          observations: [observations],
+          startDate: LocalDate.today(),
         ),
       ),
     );
   }
 }
-
-const observations = ["H", "M", "0 AD", "10 CK X2", "0 AD", "2 AD", "6CG X3"];
-final renderedObservations = renderObservations(
-  observations.map((s) => parseObservation(s)).toList(),
-  null,
-  null,
-  startDate: LocalDate.today(),
-).toList();
-final entries = observations
-    .mapIndexed(
-      (i, o) => ChartEntry(
-        observationText: o,
-        additionalText: "",
-        renderedObservation: renderedObservations[i],
-      ),
-    )
-    .toList();
-final cycle = Cycle(
-  entries: entries,
-  index: 0,
-  stickerCorrections: {},
-  observationCorrections: {},
-);
---
+++ lib/widgets/simple_chart_widget.dart
@@ -0,0 +1,61 @@
+import 'package:fc_core/logic/cycle_rendering.dart';
+import 'package:fc_core/logic/observation_parser.dart';
+import 'package:fc_core/models/chart.dart';
+import 'package:fc_core/models/observation.dart';
+import 'package:fc_core/widgets/chart_widget.dart';
+import 'package:fc_core/widgets/cycle_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:time_machine/time_machine.dart';
+
+class SimpleChartWidget extends StatefulWidget {
+  final List<List<String>> observations;
+  final LocalDate startDate;
+
+  const SimpleChartWidget({
+    super.key,
+    required this.observations,
+    required this.startDate,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _SimpleChartWidgetState();
+}
+
+class _SimpleChartWidgetState extends State<SimpleChartWidget> {
+  late ChartController _controller;
+
+  @override
+  void initState() {
+    _controller = ChartController(startOfCharting: widget.startDate);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChartWidget(
+        chart: _charts().first,
+        controller: _controller,
+        rightWidgetFn: (c) => null);
+  }
+
+  List<Chart> _charts() {
+    var currentDate = widget.startDate;
+    List<Cycle> cycles = [];
+    for (var observationStrs in widget.observations) {
+      List<Observation> observations =
+          observationStrs.map((e) => parseObservation(e)).toList();
+      var renderedObservations =
+          renderObservations(observations, null, null, startDate: currentDate);
+      currentDate = currentDate.addDays(renderedObservations.length);
+
+      var entries =
+          renderedObservations.map(ChartEntry.fromRenderedObservation).toList();
+      cycles.add(Cycle(
+          index: cycles.length,
+          entries: entries,
+          stickerCorrections: {},
+          observationCorrections: {}));
+    }
+    return Chart.fromCycles(cycles, 6);
+  }
+}
