f818e9f1573dda48b87affa7a60278dac31dc455
Parker Roth <parkeroth@gmail.com>
2025-08-31 13:45:07 -0500

    Export simple chart widget

--- lib/fc_core.dart
+++ lib/fc_core.dart
@@ -2,3 +2,5 @@ library fc_core;
 
 export 'models/instructions.dart' show Instruction;
 export 'models/instructions.dart' show Instructions;
+
+export 'widgets/simple_chart_widget.dart' show SimpleChartWidget;
